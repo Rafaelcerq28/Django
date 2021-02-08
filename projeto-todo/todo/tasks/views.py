aaa497aa38445427c435adf6625ac902ad10e0c5
rafaelcerq28 <rafaelcerq28@gmail.com>
2021-02-08 10:28:17 -0300

    inicio

---
+++ projeto-todo/todo/tasks/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def helloword(request):
+    return HttpResponse('Hello Word')
+
+def tasklist (request):
+    return render(request,'tasks/list.html')
+
+def yourName(request,name):
+    return render(request,'tasks/yourname.html',{'name':name})
